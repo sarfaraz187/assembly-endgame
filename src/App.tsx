70e24f033c5d685ae2114dc6ed8db7d9c7522e0b
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-01-17 00:58:54 +0100

    status bar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div>
       <Header />
+      <div>
+        
+      </div>
     </div>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,15 @@
 const Header = () => {
   return (
-    <header>
-      <div>Assembly: Endgame</div>
-      <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
-    </header>
+    <>
+      <header>
+        <div>Assembly: Endgame</div>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+      <section className="status-bar">
+        <h2>You Win</h2>
+        <p>Well Done!</p>
+      </section>
+    </>
   );
 };
 
