be9db69e7eea8420f413f0c7813ce65ad491c97d
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-01-17 00:45:06 +0100

    header added

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <div>Assembly: Endgame</div>
+      <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+    </header>
+  );
+};
+
+export default Header;
