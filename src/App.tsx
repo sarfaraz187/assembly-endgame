33a6ac88b5aa9b60d3be0e699e7e4e02d77d2dc0
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-01-17 18:06:03 +0100

    words display added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import "./App.css";
 import Header from "./Header";
 import { languages } from "./languages";
+import { useState } from "react";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState<string>("Hello");
+
   return (
-    <div>
+    <>
       <Header />
+      <main>
+        <div className="status-bar">
+          <h2>You Win</h2>
+          <p>Well Done!</p>
+        </div>
         <div className="language-chips">
           {languages.map((lang) => (
             <span className="chips" key={lang.name} style={{ backgroundColor: lang.backgroundColor, color: lang.color }}>
               {lang.name}
             </span>
           ))}
-      </div>
-    </div>
+        </div>
+        <div className="word-display-container">
+          {currentWord.split("").map((letter) => (
+            <span className="word-display">{letter}</span>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
--- src/Header.tsx
+++ src/Header.tsx
@@ -5,10 +5,6 @@ const Header = () => {
         <div>Assembly: Endgame</div>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </header>
-      <section className="status-bar">
-        <h2>You Win</h2>
-        <p>Well Done!</p>
-      </section>
     </>
   );
 };
