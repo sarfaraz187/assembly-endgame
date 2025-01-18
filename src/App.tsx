1e115bf11a1a27d983ce2a1841f078b92010dab7
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-01-18 16:44:13 +0100

    assemble game confetti added

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,46 @@ import Header from "./Header";
 import { languages } from "./languages";
 import { useState } from "react";
 import clsx from "clsx";
+import { getFarewellText, getRandomWord } from "./utils";
+import Confetti from "react-confetti";
 
 function App() {
-  const [currentWord, setCurrentWord] = useState<string>("react");
-  const [guessedWord, setGuessedWord] = useState<string[]>([]);
+  console.clear();
+  const [currentWord, setCurrentWord] = useState<string>(() => getRandomWord());
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+  // Derived Values
   const currentWordArr: string[] = currentWord.split("");
+  const wrongGuessCount = guessedLetters.filter((letter) => !currentWordArr.includes(letter)).length;
+  const isGameWon = currentWord.split("").every((letter) => guessedLetters.includes(letter));
+  const isGameLost = wrongGuessCount >= languages.length - 1;
+  const isGameOver = isGameWon || isGameLost;
+  const lastGuessedLetters = guessedLetters[guessedLetters.length - 1];
+  const isLastGuessIncorrect = lastGuessedLetters && !currentWordArr.includes(lastGuessedLetters);
 
   const handleBtnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     const letter = e.currentTarget.textContent?.toLowerCase();
     /**
      * Alternative method using Set which will remove duplicates
-     * const letterSet = new Set(guessedWord);
+     * const letterSet = new Set(guessedLetters);
      * letterSet.add(letter);
-     * setGuessedWord(Array.from(letterSet));
+     * setGuessedLetters(Array.from(letterSet));
      * */
-    if (letter) setGuessedWord(guessedWord.includes(letter) ? guessedWord : [...guessedWord, letter]);
+    if (letter) setGuessedLetters(guessedLetters.includes(letter) ? guessedLetters : [...guessedLetters, letter]);
   };
 
-  const handleNewGameClick = () => {
-    setCurrentWord("react");
-  };
+  // I need the count of wrong guesses to be less than 8
+  const languageElements = languages.map((lang, index) => (
+    <span key={lang.name} className={clsx({ chip: true, lost: index < wrongGuessCount })} style={{ backgroundColor: lang.backgroundColor, color: lang.color }}>
+      {lang.name}
+    </span>
+  ));
 
   const currentElements = currentWordArr.map((letter, index) => {
     return (
       <span key={index} className="word-display">
-        {guessedWord.includes(letter) ? letter.toUpperCase() : ""}
+        {isGameOver || guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
       </span>
     );
   });
@@ -37,35 +51,68 @@ function App() {
     <button
       key={index}
       onClick={handleBtnClick}
+      disabled={isGameOver}
+      aria-label={`Letter ${letter}`}
+      aria-disabled={guessedLetters.includes(letter)}
       className={clsx({
-        correct: currentWordArr.includes(letter) && guessedWord.includes(letter),
-        wrong: !currentWordArr.includes(letter) && guessedWord.includes(letter),
+        correct: currentWordArr.includes(letter) && guessedLetters.includes(letter),
+        wrong: !currentWordArr.includes(letter) && guessedLetters.includes(letter),
       })}
     >
       {letter.toUpperCase()}
     </button>
   ));
 
+  const notificationElement = () => {
+    if (!isGameOver && isLastGuessIncorrect) {
+      return <p>{getFarewellText(languages[wrongGuessCount - 1].name)}</p>;
+    }
+
+    if (isGameWon)
+      return (
+        <>
+          <h2>You Win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      );
+
+    if (isGameLost)
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      );
+  };
+
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+    farewell: !isGameOver && isLastGuessIncorrect,
+  });
+
+  function startNewGame() {
+    setCurrentWord(getRandomWord());
+    setGuessedLetters([]);
+  }
+
   return (
     <main>
+      {isGameWon && <Confetti recycle={false} numberOfPieces={1000} />}
       <Header />
-      <section className="status-bar">
-        <h2>You Win</h2>
-        <p>Well Done!</p>
+
+      <section aria-live="polite" role="status" className={gameStatusClass}>
+        {notificationElement()}
       </section>
-      <section className="language-chips">
-        {languages.map((lang) => (
-          <span className="chips" key={lang.name} style={{ backgroundColor: lang.backgroundColor, color: lang.color }}>
-            {lang.name}
-          </span>
-        ))}
-      </section>
+      <section className="language-chips">{languageElements}</section>
       <section className="word-display-container">{currentElements}</section>
       <section className="alphabet-container">{keyboardElements}</section>
 
-      <button className="new-game" onClick={handleNewGameClick}>
-        New Game
-      </button>
+      {isGameOver && (
+        <button className="new-game" onClick={startNewGame}>
+          New Game
+        </button>
+      )}
     </main>
   );
 }
